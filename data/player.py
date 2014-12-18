3853a35f3db416a9f86ded3a85bc441548889492
NestarZ <elias.rhouzlane@gmail.com>
2014-12-18 18:32:47 +0100

    Arcade/Player restructuration begins

--- data/player.py
+++ data/player.py
@@ -1,6 +1,10 @@
-class Player:
+import pygame as pg
+from . import cache
+from . import constants as c
+from .graphics.sprites import Panel, BlockGFX, CursorGFX, InfoGFX
+from .gamecore import GameCore, Cursor
 
-    INDEX = 0
+class Player:
 
     def __init__(self, index):
         self.index = index
@@ -21,9 +25,16 @@ class Player:
         cursor.add(self.cursor_gfx)
         self.alive = True
         self.score = 0
+        self.setup_board()
         self.setup_blocks()
-        self.keys_move = [keys['UP'], keys[
-            'DOWN'], keys['LEFT'], keys['RIGHT']]
+        self.setup_input()
+
+    def setup_board(self, board):
+
+        # Create graphic board thanks to logic board
+        board = BoardGFX(board)
+        board.setup_effect('fadein', 1500)
+        board.add(self.boards_group)
 
     def setup_blocks(self):
         board = self.board
@@ -32,3 +43,76 @@ class Player:
                 if board.board[row][col].color:
                     block = BlockGFX(board.board[row][col], self)
                     block.add(self.blocks_gfx)
+
+    def setup_input(self, actions):
+        actions[key.UP] = lambda: cursor.move_up()
+        actions[key.DOWN] = lambda: cursor.move_down()
+        actions[key.RIGHT] = lambda: cursor.move_right()
+        actions[key.LEFT] = lambda: cursor.move_left()
+        actions[key.SWAP] = lambda: self.swap()
+        actions[key.GENERATE] = lambda: board.geneate()
+        return actions
+
+    def check_input(self):
+        self.actions[key.UP] = lambda: cursor.move_up()
+        self.actions[key.DOWN] = lambda: cursor.move_down()
+        self.actions[key.RIGHT] = lambda: cursor.move_right()
+        self.actions[key.LEFT] = lambda: cursor.move_left()
+
+    def add_information(self, name, info=''):
+        self.info_list[name] = InfoGFX(name, info)
+        self.info_list[name].add(self.info_group)
+
+    def update_information(self, name, info=''):
+        self.info_list[name].update_information(info)
+
+    def swap(self):
+        case1, case2 = self.board.swap()
+
+    def up_row(self, player, board):
+        if board.top_row_empty():
+            board.up()
+            player.cursor.move_up()
+            cases = board.generate_hidden()
+            for c in cases:
+                block = BlockGFX(c, player)
+                block.add(player.blocks_gfx)
+        else:
+            player.alive = False
+            self.info_list.add_information('alive', 'Game Over')
+
+    def calc_new_row_timer(self):
+        return int((self.game.speed + self.pause - self.hidden_timer)/600)
+
+    def game_event(self, elapsed, player, board, destroy):
+        if destroy:
+            self.pause += destroy*1000 #x blocks = x secondes
+            self.score += destroy #x blocks = x points
+
+        info_p = "Pause={}s".format(str(int((self.pause)/600)))
+        info_r = "Up in {:.0f}s".format(self.calc_new_row_timer())
+
+        self.update_information('pause', info_p)
+        self.update_information('new_row', info_r)
+
+        if self.pause > 0:
+            self.pause -= elapsed
+        elif self.hidden_timer > self.game.speed:
+            self.up_row()
+            self.hidden_timer = 0
+        else:
+            self.hidden_timer += elapsed
+            self.pause = 0
+
+    def update(self, window, keys, elapsed):
+        self.board.gravity()
+        self.board.check_destroy()
+        self.blocks_gfx.update(elapsed, board)
+        self.cursor_gfx.update(elapsed, board)
+        self.info_gfx.update(elapsed)
+        self.rects += self.cursor_gfx.draw(window)
+        self.rects += self.blocks_gfx.draw(window)
+        self.rects += self.info_gfx.draw(window)
+        destroy = self.board.destroy_block()
+        self.board.gravity()
+        self.game_event(elapsed, player, board, destroy)
--- data/states/party.py
+++ data/states/party.py
@@ -37,6 +37,7 @@ class Arcade(Party):
     def start(self, screen, persist):
 
         # Game constants
+        # Get speed/player if passed in args with previous screens
         SPEED = persist.get('speed', 1)
         NB_PLAYER = persist.get('nb_player', 1)
         NB_COLOR = 6
@@ -56,15 +57,17 @@ class Arcade(Party):
             player.setup_game(board, gfx_board)
 
     def add_information(self, name, info=''):
+
+        # Each player will set the new information box
         for player in self.players:
-            sprite = InfoGFX(name, info)
-            sprite.add(player.info_group)
+            player.add_information(name, info)
 
     def setup_information(self, player):
 
         info = "Player {}".format(player.index)
         info = "Speed x{}".format(self.speed)
 
+        # Create useful information box for multiple usage
         self.add_information('nom', player_id)
         self.add_information('mode', mode_id)
         self.add_information('new_row')
@@ -84,114 +87,24 @@ class Arcade(Party):
         panel.setup_effect('move', 2000, (-panel.rect.h, 0))
         panel.add(self.panels_group)
 
-    def setup_board(self, board):
+    def setup_input(self):
 
-        # Create graphic board thanks to logic board
-        board = BoardGFX(board)
-        board.setup_effect('fadein', 1500)
-        board.add(self.boards_group)
-        return board
-
-    def check_input(self, keys):
-        for (i, player) in enumerate(self.players):
-            if keys[pg.K_ESCAPE]:
-                self.set_done(c.SELECT_CHAR, speed=self.speed)
-            if player.alive:
-                player.allow_input_timer += self.elapsed
-                if player.allow_input and len([key for key in keys if key != 0]):
-                    if keys[player.keys['UP']]:
-                        player.cursor.move_up()
-                    if keys[player.keys['DOWN']]:
-                        player.cursor.move_down()
-                    if keys[player.keys['RIGHT']]:
-                        player.cursor.move_right()
-                    if keys[player.keys['LEFT']]:
-                        player.cursor.move_left()
-                if player.allow_swap and keys[player.keys['SWAP']]:
-                    case1, case2 = player.board.swap()
-                    case1.swap_ongoing = True
-                    case2.swap_ongoing = True
-                if player.allow_up_row and keys[player.keys['GENERATE']]:
-                    self.up_row(player, player.board)
-
-                player.allow_input = False
-                player.allow_swap = False
-                player.allow_up_row = False
-
-                no_key_pressed = not sum(keys[key] for key in player.keys_move)
-
-                if player.allow_input_timer > 70:
-                    player.allow_input = True
-                    player.allow_input_timer = 0
-                elif no_key_pressed:
-                    player.allow_input = True
-                    player.allow_input_timer = -170
-
-                if not keys[player.keys['SWAP']]:
-                    player.allow_swap = True
-                if not keys[player.keys['GENERATE']]:
-                    player.allow_up_row = True
+        # General input handling, game inputs are set in player object
+        self.actions[pg.K_ESCAPE] = lambda: self.set_done(c.SELECT_CHAR, speed=self.speed)
 
     def set_done(self, next, **kwargs):
+
+        # Call parent class (Screen) set_done function
+        # Pass in next screen's name to display and
+        # persistant data through kwargs dictionary
         super().set_done(next, **kwargs)
-        self.to_set_done = True
-        self.to_set_done_timer = 0.5
-        self.bg.setup_effect('fadeout', 2)
 
-    def up_row(self, player, board):
-        if board.top_row_empty():
-            board.up()
-            player.cursor.move_up()
-            cases = board.generate_hidden()
-            for c in cases:
-                block = BlockGFX(c, player)
-                block.add(player.blocks_gfx)
-        else:
-            player.alive = False
-            info = "Game Over"
-            b = InfoGFX(info, player, 5)
-            b.add(player.info_gfx)
-            player.info["alive"] = b
+        # Set a countdown before state flip and apply effects
+        # on panels sprites, with same or less duration as countdown
+        self.final_countdown = c.BG_FADE_TIME
 
-    def game_event(self, elapsed, player, board, destroy):
-        if player.alive:
-            player.pause += destroy*1000
-            info = "Pause={}s".format(str(int((player.pause)/600)))
-            player.info['pause'].change_txt(info)
-            info = "Up in {:.0f}s".format(int(self.game.speed + player.pause - player.hidden_timer)/600)
-            player.info['new_row'].change_txt(info)
-            if destroy:
-                player.score += destroy
-                info = "Score={:.0f}".format(player.score)
-                player.info['score'].change_txt(info)
-            if player.pause > 0:
-                player.pause -= elapsed
-            elif player.hidden_timer > self.game.speed:
-                self.up_row(player, board)
-                player.hidden_timer = 0
-            else:
-                player.pause = 0
-                player.hidden_timer += elapsed
-
-    def update(
-        self,
-        window,
-        keys,
-        elapsed,
-    ):
+    def update(self, window, keys, elapsed):
         super().update(window, keys, elapsed)
-        self.timer += 1
-        for (i, player) in enumerate(self.players):
-            board = player.board
-            cursor = player.cursor
-            board.gravity()
-            board.check_destroy()
-            player.blocks_gfx.update(elapsed, board)
-            player.cursor_gfx.update(elapsed, board)
-            player.info_gfx.update(elapsed)
-            self.rects += player.cursor_gfx.draw(window)
-            self.rects += player.blocks_gfx.draw(window)
-            self.rects += player.info_gfx.draw(window)
-            destroy = board.destroy_block()
-            board.gravity()
-            self.game_event(elapsed, player, board, destroy)
+        self.timer += elapsed
+        for player in self.players:
+            player.update(window, keys, elapsed)
