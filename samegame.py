7c03211e98ef802beb40063aa5c4e7dc6b034088
Paul Ecoffet <ecoffet.paul@gmail.com>
2014-12-22 19:12:28 +0100

    Rename data for a more explicit and accurate name
    See guidelines there: http://infinitemonkeycorps.net/docs/pph/

--- samegame.py
+++ samegame.py
@@ -1,15 +1,15 @@
 # ==============================================================================
 """SameGame : Untitled Project"""
 # ==============================================================================
-__author__  = "Seurin Mathieu and Rhouzlane Elias"
+__author__ = "Seurin Mathieu and Rhouzlane Elias"
 __version__ = "2.1"
-__date__    = "2014-14-12"
+__date__  = "2014-14-12"
 # ==============================================================================
 #!/bin/python3
 
-from data.master import Master
-from data import constants as c
-from data.states import home, menu, party
+from samegame.master import Master
+from samegame import constants as c
+from samegame.states import home, menu, game
 
 def main():
 
@@ -18,11 +18,11 @@ def main():
                   c.MAIN_MENU : menu.Main(),
                   c.SELECT_MODE : menu.ModeSelection(),
                   c.SELECT_LEVEL : menu.LevelSelection(),
-                  c.ARCADE : party.Arcade()}
+                  c.ARCADE : game.Arcade()}
     #initalise les écrans du jeu et définit l'écran principal
     master.setup_state(state_dict, c.HOME)
-    master.main_loop() #lancement du jeu et de la boucle principale
-    master.exit() #sortie du jeu
+    master.main_loop() #  lancement du jeu et de la boucle principale
+    master.exit() #  sortie du jeu
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
