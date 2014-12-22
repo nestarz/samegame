73a199bd46889591c760db00e834b797be3acf3a
Paul Ecoffet <ecoffet.paul@gmail.com>
2014-12-22 19:17:07 +0100

    Some PEP-8 corrections

--- samegame.py
+++ samegame.py
@@ -1,9 +1,9 @@
 # ==============================================================================
-"""SameGame : Untitled Project"""
+"""SameGame: Untitled Project"""
 # ==============================================================================
 __author__ = "Seurin Mathieu and Rhouzlane Elias"
 __version__ = "2.1"
-__date__  = "2014-14-12"
+__date__ = "2014-14-12"
 # ==============================================================================
 #!/bin/python3
 
@@ -11,18 +11,17 @@ from samegame.master import Master
 from samegame import constants as c
 from samegame.states import home, menu, game
 
-def main():
 
-    master = Master() #Controleur du programme
-    state_dict = {c.HOME : home.Home(),
-                  c.MAIN_MENU : menu.Main(),
-                  c.SELECT_MODE : menu.ModeSelection(),
-                  c.SELECT_LEVEL : menu.LevelSelection(),
-                  c.ARCADE : game.Arcade()}
-    #initalise les écrans du jeu et définit l'écran principal
+def main():
+    master = Master()
+    state_dict = {c.HOME: home.Home(),
+                  c.MAIN_MENU: menu.Main(),
+                  c.SELECT_MODE: menu.ModeSelection(),
+                  c.SELECT_LEVEL: menu.LevelSelection(),
+                  c.ARCADE: game.Arcade()}
     master.setup_state(state_dict, c.HOME)
-    master.main_loop() #  lancement du jeu et de la boucle principale
-    master.exit() #  sortie du jeu
+    master.main_loop()
+    master.exit()
 
 if __name__ == '__main__':
     main()
